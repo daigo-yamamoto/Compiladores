349e39772e5549b2b2b67d15dece8422e1898558
Bruno Yamamoto <bruno.daigo.yamamoto@gmail.com>
2024-10-09 11:47:53 -0300

    feat: first part of lab (lex)

---
+++ build/CMakeFiles/tiny.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bruno-yamamoto/Documents/ita/Compiladores/TinyFlex/main.c" "CMakeFiles/tiny.dir/TinyFlex/main.c.o" "gcc" "CMakeFiles/tiny.dir/TinyFlex/main.c.o.d"
+  "/home/bruno-yamamoto/Documents/ita/Compiladores/TinyFlex/util.c" "CMakeFiles/tiny.dir/TinyFlex/util.c.o" "gcc" "CMakeFiles/tiny.dir/TinyFlex/util.c.o.d"
+  "/home/bruno-yamamoto/Documents/ita/Compiladores/lib/log.c" "CMakeFiles/tiny.dir/lib/log.c.o" "gcc" "CMakeFiles/tiny.dir/lib/log.c.o.d"
+  "/home/bruno-yamamoto/Documents/ita/Compiladores/build/tinylexer.c" "CMakeFiles/tiny.dir/tinylexer.c.o" "gcc" "CMakeFiles/tiny.dir/tinylexer.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
