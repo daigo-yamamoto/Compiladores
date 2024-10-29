fe4a28155a1f66c146cc0130ea20f65a0e6ee676
Bruno Yamamoto <bruno.daigo.yamamoto@gmail.com>
2024-10-29 11:14:22 -0300

    Remove alunodetail e build do rastreamento

--- build/CMakeFiles/lexdiff.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lexdiff"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lexdiff.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/runmycmcomp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/runmycmcomp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/runmycmcomp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
